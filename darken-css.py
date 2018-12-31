eba9a73593e092d064f946c8736e9877f321d3f2
Naheel-Azawy <naheelazawy@gmail.com>
2019-01-01 02:37:32 +0300

    quick update

--- darken-css.py
+++ darken-css.py
@@ -5,11 +5,11 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Darken CSS style sheets")
 parser.add_argument('--darkness', help='How much darkness to be added',
-                    metavar="N", type=int, default=7)
+                    metavar="N", type=float, default=7)
 parser.add_argument('--sd-thr', help='Standard deviation threshold. The higher the more colorful colors will be darkened (0 will only darken grayscale colors)',
-                    metavar="N", type=int, default=2)
+                    metavar="N", type=float, default=2)
 parser.add_argument('--avg-thr', help='Average threshold. The higher the more light colors will be dimmed',
-                    metavar="N", type=int, default=80)
+                    metavar="N", type=float, default=80)
 
 args = parser.parse_args()
 
@@ -21,37 +21,38 @@ def hex2str(i):
     return "#%06x" % i
 
 def rgba2str(r, g, b, a):
-    return "rgba(%d, %d, %d, %d)" % (r, g, b, a)
+    return "rgba(%d, %d, %d, %s)" % (r, g, b, a)
 
 for line in sys.stdin:
-    m = re.match(r'.*#([\da-fA-F]{6}).*', line)
+    m = re.findall(r'#([\da-fA-F]{6})', line)
     if m:
-        c = int(m.group(1), 16)
-        r = c >> 16
-        g = (c >> 8) & 0xff
-        b = c & 0xff
-        sd = statistics.stdev([r, g, b])
-        if sd < sd_thr: # grayscale (sd = 0) or sort of grayscale
-            avg = int((r + g + b) / 3)
-            if avg < avg_thr:
-                avg = int(avg / darkness) # darken
-                r = g = b = avg # force grayscale
-                c2 = (r << 16) + (g << 8) + b
-                line = line.replace(hex2str(c), hex2str(c2))
-    m = re.match(r'.*(rgba\(\d+\s*,\s*\d+\s*,\s*\d+\s*,\s*\d+\s*\)).*', line)
+        for color_str in m:
+            c = int(color_str, 16)
+            r = c >> 16
+            g = (c >> 8) & 0xff
+            b = c & 0xff
+            sd = statistics.stdev([r, g, b])
+            if sd < sd_thr: # grayscale (sd = 0) or sort of grayscale
+                avg = int((r + g + b) / 3)
+                if avg < avg_thr:
+                    avg = int(avg / darkness) # darken
+                    r = g = b = avg # force grayscale
+                    c2 = (r << 16) + (g << 8) + b
+                    line = line.replace(hex2str(c), hex2str(c2))
+    m = re.findall(r'(rgba\(\d+\s*,\s*\d+\s*,\s*\d+\s*,\s*[\d.]+\s*\))', line)
     if m:
-        color_str = m.group(1)
-        m = re.match(r'rgba\((\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)', color_str)
-        r = int(m.group(1))
-        g = int(m.group(2))
-        b = int(m.group(3))
-        a = int(m.group(4))
-        sd = statistics.stdev([r, g, b])
-        if sd < sd_thr: # grayscale (sd = 0) or sort of grayscale
-            avg = int((r + g + b) / 3)
-            if avg < avg_thr:
-                avg = int(avg / darkness) # darken
-                r = g = b = avg # force grayscale
-                line = line.replace(color_str, rgba2str(r, g, b, a))
+        for color_str in m:
+            m = re.findall(r'rgba\((\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*,\s*([\d.]+)\s*\)', color_str)
+            r = int(m[0][0])
+            g = int(m[0][1])
+            b = int(m[0][2])
+            a = m[0][3]
+            sd = statistics.stdev([r, g, b])
+            if sd < sd_thr: # grayscale (sd = 0) or sort of grayscale
+                avg = int((r + g + b) / 3)
+                if avg < avg_thr:
+                    avg = int(avg / darkness) # darken
+                    r = g = b = avg # force grayscale
+                    line = line.replace(color_str, rgba2str(r, g, b, a))
     print(line[:-1])
 
